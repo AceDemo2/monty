4188fd50b5fcbe398b280501344965015680517b
AceDemo2 <lawaladepoju705@gmail.com>
2024-04-16 14:53:06 -0700

    stackes and queues

--- main.c
+++ main.c
@@ -2,67 +2,66 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- *  * main - Entry point
- *   * @argc: Argument count
- *    * @argv: Argument vector
- *     * Return: EXIT_SUCCESS on success, EXIT_FAILURE on failure
- *      */
 int main(int argc, char *argv[])
 {
-	    if (argc != 2)
-		        {
-				        fprintf(stderr, "Usage: %s <file>\n", argv[0]);
-					        exit(EXIT_FAILURE);
-						    }
+	    FILE *file;
+	        stack_t *stack = NULL;
+		    char *line = NULL;
+		        size_t len = 0;
+			    ssize_t read;
+			        unsigned int line_number = 0;
+				    char *opcode;
+				        char *arg;
+					    int value;
 
-	        FILE *file = fopen(argv[1], "r");
-		    if (file == NULL)
-			        {
-					        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-						        exit(EXIT_FAILURE);
-							    }
+					        if (argc != 2)
+							    {
+								            fprintf(stderr, "Usage: %s <file>\n", argv[0]);
+									            exit(EXIT_FAILURE);
+										        }
 
-		        stack_t *stack = NULL;
-			    char *line = NULL;
-			        size_t len = 0;
-				    ssize_t read;
-				        unsigned int line_number = 0;
+						    file = fopen(argv[1], "r");
+						        if (file == NULL)
+								    {
+									            fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+										            exit(EXIT_FAILURE);
+											        }
 
-					    while ((read = getline(&line, &len, file)) != -1)
-						        {
-								        line_number++;
-									        char *opcode = strtok(line, " \t\n");
-										        char *arg = strtok(NULL, " \t\n");
+							    while ((read = getline(&line, &len, file)) != -1)
+								        {
+										        opcode = strtok(line, " \t\n");
+											        arg = strtok(NULL, " \t\n");
 
-											        if (opcode == NULL || (strcmp(opcode, "push") == 0 && arg == NULL))
-													        {
-															            fprintf(stderr, "L%d: usage: push integer\n", line_number);
-																                exit(EXIT_FAILURE);
-																		        }
-
-												        if (strcmp(opcode, "push") == 0)
+												        if (opcode == NULL || (strcmp(opcode, "push") == 0 && arg == NULL))
 														        {
-																            int value = atoi(arg);
-																	                _push(&stack, value);
+																            fprintf(stderr, "L%d: usage: push integer\n", line_number);
+																	                exit(EXIT_FAILURE);
 																			        }
-													        else if (strcmp(opcode, "pall") == 0)
+
+													        if (strcmp(opcode, "push") == 0)
 															        {
-																	            _pall(stack);
-																		            }
-														        else
+																	            value = atoi(arg);
+																		                _push(&stack, value);
+																				        }
+														        else if (strcmp(opcode, "pall") == 0)
 																        {
-																		            fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
-																			                exit(EXIT_FAILURE);
-																					        }
-															    }
+																		            _pall(stack);
+																			            }
+															        else
+																	        {
+																			            fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
+																				                exit(EXIT_FAILURE);
+																						        }
 
-					        fclose(file);
-						    if (line)
-							        {
-									        free(line);
-										    }
+																        line_number++;
+																	    }
 
-						        return EXIT_SUCCESS;
+							        fclose(file);
+								    if (line)
+									        {
+											        free(line);
+												    }
+
+								        return EXIT_SUCCESS;
 }
 
